56b8fa0bab167caa03ca6398fb75d989c5719b2d
rahmandiehard <rahman_diehard@yahoo.co.in>
2022-10-24 15:04:46 +0530

    first push

---
+++ src/main/java/com/tech/aws/Elasticbeanstalkexample/ElasticbeanstalkExampleApplication.java
@@ -0,0 +1,15 @@
+package com.tech.aws.Elasticbeanstalkexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticbeanstalkExampleApplication {
+	public ElasticbeanstalkExampleApplication() {
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticbeanstalkExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/aws/Elasticbeanstalkexample/ElasticbeanstalkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.aws.Elasticbeanstalkexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticbeanstalkExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
